f772469ce39bdde6db72fd8cb9be2ad836682b2e
khalid <khalid.rfk@gmail.com>
2021-06-16 09:56:02 +0100

    cc

---
+++ src/tmp.ts
@@ -0,0 +1,10 @@
+import { SchemaDirectiveVisitor } from "apollo-server";
+import { GraphQLInputObjectType, GraphQLObjectType } from "graphql";
+
+a: GraphQLInputObjectType;
+
+GraphQLObjectType
+GraphQLInputObjectType
+
+
+SchemaDirectiveVisitor
